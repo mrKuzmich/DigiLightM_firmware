39eb41dacfcd8d0e494179cb173a80ee1de91395
Dmitry Kuzmin <kuzmich.murom@gmail.com>
2023-11-27 20:39:04 +0300

    Changed maximum effects count

--- src/main_effect.h
+++ src/main_effect.h
@@ -43,7 +43,7 @@
 #include "sd_script.h"
 
 /// предельное количество эффектов любого типа
-#define MAX_EFFECT_CNT		6
+#define MAX_EFFECT_CNT		7
 /// фоновый эффект
 #define BACKGROUND_EFFECT	0
 /// основной эффект
